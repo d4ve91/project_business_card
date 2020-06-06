ad5944d1feee43541eae39d64ddb493b77acdc3f
Dawid Pytlinski <pytlinski.david@gmail.com>
2020-06-06 20:24:34 +0200

    Add new file

---
+++ business_card.py
@@ -0,0 +1,20 @@
+
+name = input("Give me your name: ")
+#print("Your name is: " + name)
+
+surname = input("Give me your surname: ")
+#print("Your surname is: " + surname)
+
+age = input("Give me your age: ")
+#print("Your age is: " + age)
+
+phone_number = input("Give me your phone number: ")
+#print("Your number is: " + phone_number)
+
+email = input("Give me your email address: ")
+#print("Your email address is: " + email)
+
+print("\n" + "*" *30 + "\n")
+print("Your name is: " + name + "\n" + "Your surname is: " + surname + "\n" + "Your age is: " + age +
+      "\n" + "Your phone number is: " + phone_number + "\n" + "Your email address is: " + email)
+print("\n" + "*" *30 + "\n")
